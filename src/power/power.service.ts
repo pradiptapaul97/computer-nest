bb0c04aa9e52ca10c3e5a952ddddf952ff73d6a6
Pradipta Paul <pradipta.paul@webskitters.com>
2024-10-13 01:06:29 +0530

    first commit

---
+++ src/power/power.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PowerService {
+
+    supplyPower(watts: number)
+    {
+        console.log(`POWER SERVICE => Supplying ${watts} worth of power`);
+        
+    }
+}
